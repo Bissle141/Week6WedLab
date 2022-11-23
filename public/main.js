cc49a5398987dc1005a370c13d13e5135315f5c8
Alyssa <alyssa.black141@gmail.com>
2022-11-23 14:57:38 -0700

    added 2nd button functionality

--- public/main.js
+++ public/main.js
@@ -3,3 +3,19 @@ const btn = document.querySelector('button')
 const clickHandler = () => alert('The cat name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
+
+const btn2 = document.querySelector('#button2')
+
+const url = 'http://localhost:4000'
+
+const getCat = () => {
+    axios.get(`${url}/api/cat`)
+    .then(res => {
+        alert(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
+btn2.addEventListener('click', getCat)
